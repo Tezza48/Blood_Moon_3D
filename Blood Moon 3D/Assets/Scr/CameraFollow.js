d7e9f0fef74704dcc2f8e4666cc35c6fbbec63c8
William Terry <Tezza48is@aol.com>
2015-11-23 01:29:55 +0000

    initialising git in blood moon 3D

---
+++ Blood Moon 3D/Assets/Scr/CameraFollow.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+private var player : GameObject;
+
+function Start () {
+      player = GameObject.FindGameObjectWithTag("Player");
+}
+
+function Update () {
+      transform.position = player.transform.position + new Vector3(0, 2, -2.5);
+}
---
+++ Blood Moon 3D/Assets/Scr/PlayerMovement.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+private var moon : GameObject;
+
+function Start () {
+      moon = GameObject.Find("Moon");
+}
+
+function Update () {
+      moon.GetComponent.<Rigidbody>().rotation += new Quaternion.euler ( Input.GetAxis ( "Horizontal" ), 0.0, Input.GetAxis ( "Vertical" ) );
+}
