89196b7b8476596994c5ae4ede3f3790f4669117
William Terry <Tezza48is@aol.com>
2015-11-26 15:37:16 +0000

    still borked but better
    locks to x -90 or +90

--- Blood Moon 3D/Assets/Scr/PlayerMovement.js
+++ Blood Moon 3D/Assets/Scr/PlayerMovement.js
@@ -1,11 +1,26 @@
 ﻿#pragma strict
 
 private var moon : GameObject;
+private var moonRigid : Rigidbody;
+private var torque : float = 10;
 
 function Start () {
       moon = GameObject.Find("Moon");
+      moonRigid = moon.GetComponent.<Rigidbody>();
 }
 
 function Update () {
-      moon.GetComponent.<Rigidbody>().rotation += new Quaternion.euler ( Input.GetAxis ( "Horizontal" ), 0.0, Input.GetAxis ( "Vertical" ) );
+	
+	var rotX = -Input.GetAxis ( "Vertical" ) * torque * Time.deltaTime;
+	var rotY = Input.GetAxis ( "Horizontal" ) * torque * Time.deltaTime;
+	
+	var currentX = moonRigid.rotation.eulerAngles.x;
+	var currentY = moonRigid.rotation.eulerAngles.y;
+	var currentZ = moonRigid.rotation.eulerAngles.z;
+	
+	// var currentRot : Vector3 = new Vector3(currentX, currentY, currentZ);
+	
+	var newRot : Quaternion = Quaternion.Euler(transform.rotation.x + currentX + rotX, 0, 0);
+	
+	moonRigid.MoveRotation(newRot);
 }
